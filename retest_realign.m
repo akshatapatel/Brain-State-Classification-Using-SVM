2634e20a694e3d0e4eb2f0a3a673a2eb96755cef
Akshata Patel <akshatapatel_95@yahoo.co.in>
2019-01-17 20:57:55 -0500

    Matlab files

---
+++ retest_realign.m
@@ -0,0 +1,18 @@
+%-----------------------------------------------------------------------
+% Job saved on 28-Nov-2018 23:51:01 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7487)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.spatial.realign.estwrite.data = {{'/Users/akshatapatel/Documents/Columbia/Fall2018/ImageAnalysis/Assignment3/sub-01/ses-retest/func/sub-01_ses-retest_task-fingerfootlips_bold.nii,1'}};
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.quality = 0.9;
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.sep = 4;
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.fwhm = 5;
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.rtm = 1;
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.interp = 2;
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.wrap = [0 0 0];
+matlabbatch{1}.spm.spatial.realign.estwrite.eoptions.weight = '';
+matlabbatch{1}.spm.spatial.realign.estwrite.roptions.which = [2 0];
+matlabbatch{1}.spm.spatial.realign.estwrite.roptions.interp = 4;
+matlabbatch{1}.spm.spatial.realign.estwrite.roptions.wrap = [0 0 0];
+matlabbatch{1}.spm.spatial.realign.estwrite.roptions.mask = 1;
+matlabbatch{1}.spm.spatial.realign.estwrite.roptions.prefix = 'r';
